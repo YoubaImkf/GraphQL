a2879e55e8adcd4d83e7a7205806467031aa9614
Youba IMAKHLAF <youba.imakhlaf@modiscloud.net>
2024-02-19 12:29:58 +0100

    🎉 Inital commit

---
+++ First-GraphQL-project/src/App.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}`);
+      const data = await response.json();
+      setCharacters(data.results);
+    }
+    fetchCharacters();
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1);
+  };
+
+  const handleCharacterClick = async (characterId) => {
+    try {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`);
+      const data = await response.json();
+      setSelectedCharacter(data);
+    } catch (error) {
+      console.error('Error fetching character details:', error);
+    }
+  };
+
+  return (
+    <div className='list-content'>
+      <h1>Rick & Morty Characters List</h1>
+      <ul className="character-grid">
+        {characters.map(character => (
+          <li key={character.id} onClick={() => handleCharacterClick(character.id)}>
+            <img 
+              src={character.image} 
+              alt={character.name}
+            />
+            <p>{character.name}</p>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleNextPage}>Next Pageeeeeee</button>
+
+      {/* Modal for character details */}
+      {selectedCharacter && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setSelectedCharacter(null)}>&times;</span>
+            <h2>{selectedCharacter?.name}</h2>
+            <img 
+              src={selectedCharacter?.image} 
+              alt={selectedCharacter?.name} 
+            />
+            <p>{selectedCharacter.type}</p>
+            <p>{selectedCharacter.status}</p>
+            <p>{selectedCharacter.species}</p>
+            <p>{selectedCharacter.gender}</p>
+          </div>
+        </div>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
